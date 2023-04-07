ccc2014b14d6004280c0c68936f9e9f5025abe73
Keara Harris <keara.s.harris@gmail.com>
2023-04-07 01:34:45 -0500

    Completed Functions-continued lab, anonymous functions, innerfunctions, scope

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// saturdayFun w/ default parameter
+function saturdayFun(saturdayActivity = "roller-skate"){
+    let activityMessage = `This Saturday, I want to ${saturdayActivity}!`
+    return activityMessage;
+}
+
+//mondayWork w/ default parameters and function expressions
+
+const mondayWork = function(workActivity= "go to the office"){
+    let workMessage = `This Monday, I will ${workActivity}.`
+    return workMessage;
+}
+//wrapAdjective, scope, annoymous functions, inner functuons
+
+function wrapAdjective(visualFlair){
+    const wrapAdjectiveMessage = function (adjective = "special"){
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+    return wrapAdjectiveMessage;
+
+}
